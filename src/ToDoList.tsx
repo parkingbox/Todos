edc22a8f2f9dfaa0ce6da248b1c4225d6b226097
Seungwoo Park <qpfgud430@gmail.com>
2022-10-10 05:11:41 +0900

    feat: Create validation using react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -17,7 +17,7 @@ import { useForm } from "react-hook-form";
 //       }
 //       console.log("submit");
 //     }
-//   return 
+//   return
 //   <div>
 //     <form>
 //       <input onChange={onChange} value={toDo} placeholder="Write a to do" />
@@ -29,81 +29,97 @@ import { useForm } from "react-hook-form";
 
 type IFormData = {
   errors: {
-  email: {
-  message: string;
-  };
+    email: {
+      message: string;
+    };
   };
   firstName: string;
   lastName: string;
   email: string;
   password: string;
   CheckingPassword: string;
-  };
+  extraError?: string;
+};
 
 function ToDoList() {
-  const { register,handleSubmit, formState:{errors} } = useForm<IFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IFormData>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
-  }
-  
-  return ( 
+  const onValid = (data: IFormData) => {
+    if (data.password !== data.CheckingPassword) {
+      setError(
+        "CheckingPassword",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "Server offline."})
+  };
+  console.log(errors);
+
+  return (
     <div>
-      <form 
+      <form
         style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={ handleSubmit(onValid) }
+        onSubmit={handleSubmit(onValid)}
       >
         <input
-          { ...register("email", {
-            required:"Email is required",
+          {...register("email", {
+            required: "Email is required",
             pattern: {
               value: /^[A-Za-z0-9._%+-]+@naver.com$/,
               message: "Only naver.com emails allowed",
-
             },
-          })} placeholder="Email"
+          })}
+          placeholder="Email"
         />
-        <span>
-          {errors?.email?.message}
-        </span>
-        <input { ...register("firstName", { required: "write here" })}
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noWoo: (value) =>
+                value.includes("woo") ? "no woo allowed" : true,
+              noSeung: (value) =>
+                value.includes("Seung") ? "no woo allowed" : true,
+            },
+          })}
           placeholder="firstName"
         />
-        <span>
-          {errors?.firstName?.message}
-        </span>
-        <input { ...register("lastName", { required: "write here" })}
-          placeholder="Email"
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="lastName"
         />
-        <span>
-          {errors?.lastName?.message}
-        </span>
-        <input { ...register("password",{ required: "write here", minLength: 5 })}
-          placeholder="Email"
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="password"
         />
-        <span>
-          {errors?.password?.message}
-        </span>
-        <input {
-          ...register("CheckingPassword", {
-            required:"Passwored is required",
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("CheckingPassword", {
+            required: "Passwored is required",
             minLength: {
-              value:5,
+              value: 5,
               message: "Your password is too short.",
             },
           })}
           placeholder="CheckingPassword"
         />
-        <span>
-          {errors?.CheckingPassword?.message}
-        </span>
+        <span>{errors?.CheckingPassword?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
-  )
+  );
 }
 
 export default ToDoList;
