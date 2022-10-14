933e1a1f925bdd74009b456bd33a6fe598360b26
Seungwoo Park <qpfgud430@gmail.com>
2022-10-15 00:42:25 +0900

    feat: Create todolist using selector

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,5 @@
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "./atoms";
-
-const animals = ["ant", "bison", "duck", "elephant"];
+import { IToDo, toDoState, Categories } from "./atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
@@ -23,18 +21,18 @@ function ToDo({ text, category, id }: IToDo) {
   return (
     <li>
       <span>{text}</span>
-      {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO + ""} onClick={onClick}>
           To Do
         </button>
       )}
-      {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING + ""} onClick={onClick}>
           Doing
         </button>
       )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE + ""} onClick={onClick}>
           Done
         </button>
       )}
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { toDoState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector, toDoState, Categories } from "./atoms";
 import CreateToDo from "./CreateToDos";
 import ToDo from "./ToDo";
 
-
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
-  
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
+  };
+
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <form action="">
+        <select value={category} onInput={onInput}>
+          <option value={Categories.TO_DO}>To DO</option>
+          <option value={Categories.DOING}>Doing</option>
+          <option value={Categories.DONE}>Done</option>
+        </select>
+      </form>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id}{...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
